94962f7dafd58d861ee49a6d22b6f752544e1ead
cashely <290119516@qq.com>
2017-05-30 22:11:45 +0800

    2017-05-30 01

--- modules/pdf.js
+++ modules/pdf.js
@@ -2,35 +2,89 @@
 let titleSize = 10;
 let textSize = 8;
 let size_18 = 18;
+let size_12 = 10;
+let defaultColor = '#333';
+let themeColor = '#3385ff';
 let font = 'fonts/msyh.ttf';
 function getWidth(doc){
     console.log(doc.page.width);
     return doc.page.width;
 }
+function text(doc,text){
+    return doc
+            .font(font)
+            .fillColor(defaultColor)
+            .fontSize(textSize)
+            .text(text)
+}
+
+function weight(doc,text){
+    return  doc
+                .font(font)
+                .fillColor(defaultColor)
+                .fontSize(size_12)
+                .text(text)
+}
+function work(doc,title,content){
+    // return function(){
+        weight(doc,title);
+        text(doc,content);
+        doc.moveDown();
+    // }
+}
+
+function works(doc,worksMap){
+    worksMap.forEach((item)=>{
+        work(doc,item.title,item.content)
+    })
+}
 module.exports = {
     title:function (doc,text,x,y){
-        console.log('title');
+
         return ()=>{
+                  doc.moveUp();
                   doc
                   .rect(x,y,getWidth(doc)-(x*2),titleSize+4)
-                  .fill('#3385ff')
+                  .fill(themeColor)
                   .font(font)
                   .fontSize(titleSize)
                   .fillColor('#ffffff')
-                  .text(text,x+10,y);
+                  .text(text,x+5,y);
+                  doc.moveDown();
               }
     },
+    work:work,
+    works:works,
+    school:work,
+    schools:works,
     h2:(doc,text,x,y)=>{
         return doc
                 .font(font)
                 .fontSize(size_18)
                 .text(text,x,y);
     },
+    weight:(doc,text)=>{
+        return  doc
+                    .font(font)
+                    .fontSize(size_12)
+                    .text(text)
+    },
     getWidth:getWidth,
-    text:(doc,text)=>{
-        return doc
-                .font(font)
-                .fontSize(textSize)
-                .text(text)
+    text:text,
+    ul:(doc,words)=>{
+        let p = words.split('\n');
+        return p.forEach((item,index)=>{
+            text(doc,item);
+        })
+    },
+    skillMap:(doc,words)=>{
+        let arr = words.split(',');
+        return arr.forEach((item,index)=>{
+
+            doc.rect(doc.x+20+(index !=0 ? doc.widthOfString(arr[index-1]) : 0),doc.y-(index != 0 ? doc.currentLineHeight() : 0),doc.widthOfString(item)+10,doc.currentLineHeight()+2).fill('grey');
+            // text(doc,item);
+            doc.fillColor('#ffffff').text(item,doc.x+25+(index !=0 ? doc.widthOfString(arr[index-1]) : 0),doc.y-(index != 0 ? doc.currentLineHeight() : 0));
+
+        })
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -22,9 +22,48 @@ router.get('/pdf',function(req,res,next){
     doc.moveDown(0.4);
     makePdf.text(doc,'手机：15013306010');
     makePdf.text(doc,'Email：290119516@qq.com');
-    doc.image('public/images/873b57828b3a0a67-77cabb970fa5d135-f7440e1fa71165595d808201d2aa9c50.jpg',doc.page.width-100,10,{width:80,height:100});
+    doc.image('public/images/873b57828b3a0a67-77cabb970fa5d135-f7440e1fa71165595d808201d2aa9c50.jpg',doc.page.width-90,10,{width:80,height:100});
     doc.moveDown(2);
-    makePdf.title(doc,'我的资料',10,doc.y)();
+    makePdf.title(doc,'自我评价',10,doc.y)();
+    makePdf.ul(doc,'1、精通 JavaScript HTML/HTML5/XML、CSS/CSS3，熟悉常见跨浏览器问题，了解必要的计算机网络协议；\n2、熟悉主流JavaScript框架和库，理解框架设计原理，目前主要致力于react全家桶的研究使用，有良好的WEB前端优化经验；\n3、熟悉HTML5等前端领域新技，熟悉W3C标准、Ajax、DOM、BOM等技术，了解angularjs/react/react native等Javascript开发架构和组件;')
+
+    doc.moveDown();
+    makePdf.title(doc,'求职意向',10,doc.y)();
+
+    doc.moveDown();
+    makePdf.title(doc,'技能标签',10,doc.y)();
+    makePdf.skillMap(doc,'js,html,css3,jquery')
+
+
+    doc.moveDown();
+    makePdf.title(doc,'工作经历',10,doc.y)();
+    makePdf.works(doc,[
+                        {title:'广州责卓医药咨询有限公司 | 25000k/月',content:'责任描述：1、网站整体、页面布局规划；'},
+                        {title:'广州责卓医药咨询有限公司 | 25000k/月',content:'责任描述：1、网站整体、页面布局规划；'},
+                        {title:'广州责卓医药咨询有限公司 | 25000k/月',content:'责任描述：1、网站整体、页面布局规划；'}
+                    ]);
+
+
+    doc.moveDown();
+    makePdf.title(doc,'教育经历',10,doc.y)();
+    makePdf.schools(doc,[
+                            {title:'广东工贸职业技术学院 电脑艺术设计 大专',content:'主要学习的是一些基础知识;'},
+                            {title:'广东工贸职业技术学院 电脑艺术设计 大专',content:'主要学习的是一些基础知识;'},
+                            {title:'广东工贸职业技术学院 电脑艺术设计 大专',content:'主要学习的是一些基础知识;'}
+
+                    ]);
+
+
+
+
+
+
+
+
+
+
+
+
     doc.end();
 
     resultPdf.on('finish',()=>{
